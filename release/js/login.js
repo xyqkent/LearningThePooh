77209c049d90874c729e71d18aa4e53dab3261aa
xyqkent <xyqkent@163.com>
2021-08-24 20:00:39 +0800

    1/修复app扫码登陆不弹出问题 2/修复自动显示二维码居中

--- release/js/login.js
+++ release/js/login.js
@@ -1,17 +1,19 @@
-let qrCode = document.querySelector('.ddloginbox');
-if (qrCode) {
-    setTimeout(function () {
-        qrCode.scrollIntoView({
-            behavior: "smooth"
-        });
+setTimeout(function () {
+    let qrCode = document.querySelector('.ddloginbox');
+    if (qrCode) {
         setTimeout(function () {
-            window.scrollTo({
-                left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
-                top: window.scrollY,
-                behavior: 'smooth'
+            qrCode.scrollIntoView({
+                behavior: "smooth"
             });
-        }, 1000 + Math.floor(Math.random() * 1000));
-    }, 1000 + Math.floor(Math.random() * 1000))
-}
+            setTimeout(function () {
+                window.scrollTo({
+                    left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
+                    top: window.scrollY,
+                    behavior: 'smooth'
+                });
+            }, 1000 + Math.floor(Math.random() * 1000));
+        }, 1000 + Math.floor(Math.random() * 1000))
+    }
+}, 1000 + Math.floor(Math.random() * 1000))
 
 chrome.runtime.sendMessage({"method": "checkLogin"});
